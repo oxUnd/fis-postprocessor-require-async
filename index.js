46382c2a18bc992793b1046cb74df1f315e2ab41
xiangshouding <fansekey@gmail.com>
2013-11-21 16:16:44 +0800

    js注视语法支持require.async

--- index.js
+++ index.js
@@ -12,6 +12,42 @@ function pregQuote (str, delimiter) {
     return (str + '').replace(new RegExp('[.\\\\+*?\\[\\^\\]$(){}=!<>|:\\' + (delimiter || '') + '-]', 'g'), '\\$&');
 }
 
+
+function addAsync(file, value) {
+    var hasBrackets = false;
+    var values = [];
+    value = value.trim().replace(/(^\[|\]$)/g, function(m, v) {
+        if (v) {
+            hasBrackets = true;
+        }
+        return '';
+    });
+    values = value.split(/\s*,\s*/);
+    values = values.map(function(v) {
+        var info = fis.util.stringQuote(v);
+        v = info.rest.trim();
+        var uri = fis.uri.getId(v, file.dirname);
+        if (file.extras.async.indexOf(uri.id) < 0) {
+            file.extras.async.push(uri.id);
+        }
+        return info.quote + uri.id + info.quote;
+    });
+
+    return {
+        values: values,
+        hasBrackets: hasBrackets
+    };
+}
+
+//analyse [@require.async id] syntax in comment
+function analyseComment(file, comment){
+    var reg = /(@require\.async\s+)('[^']+'|"[^"]+"|[^\s;!@#%^&*()]+)/g;
+    return comment.replace(reg, function(m, prefix, value){
+        addAsync(file, value);
+        return '';
+    });
+}
+
 //require.async(path) to require resource
 function parseJs(content, file, conf){
     var reg = /"(?:[^\\"\r\n\f]|\\[\s\S])*"|'(?:[^\\'\n\r\f]|\\[\s\S])*'|(\/\/[^\r\n\f]+|\/\*[\s\S]+?(?:\*\/|$))|\b(require\.async)\s*\(\s*("(?:[^\\"\r\n\f]|\\[\s\S])*"|'(?:[^\\'\n\r\f]|\\[\s\S])*'|\[[\s\S]*?\])\s*/g;
@@ -19,31 +55,16 @@ function parseJs(content, file, conf){
         if(type){
             switch (type){
                 case 'require.async':
-                    var hasBrackets = false;
-                    var values = [];
-                    value = value.trim().replace(/(^\[|\]$)/g, function(m, v) {
-                        if (v) {
-                            hasBrackets = true;
-                        }
-                        return '';
-                    });
-                    values = value.split(/\s*,\s*/);
-                    values = values.map(function(v) {
-                        var info = fis.util.stringQuote(v);
-                        v = info.rest.trim();
-                        var uri = fis.uri.getId(v, file.dirname);
-                        if (file.extras.async.indexOf(uri.id) < 0) {
-                            file.extras.async.push(uri.id);
-                        }
-                        return info.quote + uri.id + info.quote;
-                    });
-                    if (hasBrackets) {
-                        m = 'require.async([' + values.join(', ') + ']';
+                    var res = addAsync(file, value);
+                    if (res.hasBrackets) {
+                        m = 'require.async([' + res.values.join(', ') + ']';
                     } else {
-                        m = 'require.async(' + values.join(', ');
+                        m = 'require.async(' + res.values.join(', ');
                     }
                     break;
             }
+        } else if (comment) {
+            m = analyseComment(file, comment);
         }
         return m;
     });
